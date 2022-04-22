187faa3707a0b846c206a3c82a7d6dfd02a2b708
Niklas Lübcke <niklas.luebcke@innoplexia.com>
2022-04-22 10:49:55 +0200

    Revert "MongoDB and Neo4j"
    This reverts commit 6c785a3c9f0717730a93121e4c4e4a5da6d420c7.

--- connect-with-rust/src/main.rs
+++ connect-with-rust/src/main.rs
@@ -1,13 +1,3 @@
-use rusted_cypher::GraphClient;
-//use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
-use std::env;
-
 fn main() {
-    println!("starting...");
-
-    let neo4j_connection = GraphClient::connect("neo4j://localhost:7687");
-    let mongo_connection = env::var("mongodb://localhost:27017").expect("You must set the MONGODB_URI environment var!");
-
-
-    println!("Done!");
+    println!("Hello, world!");
 }
