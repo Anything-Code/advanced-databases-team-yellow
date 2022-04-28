dd2772719132f5ca36c5dc68137ed291b5549c03
Niklas Lübcke <niklas.luebcke@innoplexia.com>
2022-04-28 14:07:43 +0200

    Adds interpolation to parsed line_string in rust.

--- connect-with-rust/src/main.rs
+++ connect-with-rust/src/main.rs
@@ -1,17 +1,20 @@
 // mod connect_mongodb;
 // mod connect_neo4j;
 mod connect_redis;
+mod paths;
 
 use std::error::Error;
 
 // #[tokio::main]
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Starting...");
+    println!("Starting...\n");
 
     // connect_mongodb::test().await?;
     // connect_neo4j::test().await;
-    connect_redis::test()?;
 
-    println!("Done!");
+    // connect_redis::test()?;
+    paths::read_kml();
+
+    println!("\nDone!");
     Ok(())
 }
