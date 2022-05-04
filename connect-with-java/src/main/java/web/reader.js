2c3a527bbf583081086b4794fc2cd6b4956934a6
Emil-Linnebjerg <43955067+Emil-Linnebjerg@users.noreply.github.com>
2022-05-04 17:05:30 +0200

    changed the way true way api is handled
    Now it uses JSON objects yay

--- connect-with-java/src/main/java/web/reader.js
+++ connect-with-java/src/main/java/web/reader.js
@@ -1,17 +1,9 @@
 const citymap = {
-40: {
-center: { lat: 40.39759, lng: -80.085621 },
-population: 10,
-},
-44: {
-center: { lat: 40.39759, lng: -80.085621 },
-population: 10,
-},
-45: {
+11: {
 center: { lat: 49.404222, lng: 8.66702 },
 population: 10,
 },
-46: {
+12: {
 center: { lat: 49.395501, lng: 8.643643 },
 population: 10,
 },
