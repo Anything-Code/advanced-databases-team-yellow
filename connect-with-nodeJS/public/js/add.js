30df5bfb25e800dc38ad28027bda0181203f184d
Enes AGIR <enes.agir.7@gmail.com>
2022-05-05 16:24:53 +0200

    volunteer address
    added done

---
+++ connect-with-nodeJS/public/js/add.js
@@ -0,0 +1,40 @@
+const volunteerForm = document.getElementById('volunteer-form');
+const volunteerId = document.getElementById('volunteer-id');
+const volunteerAddress= document.getElementById('volunteer-address');
+
+// send POST to API to add volunteer
+async function addVolunteer(e){
+    e.preventDefault();
+
+    if(volunteerId.value ==='' || volunteerAddress.value===''){
+        alert('Please fill in fields');
+    }
+
+    const sendBody ={
+        volunteerId: volunteerId.value,
+        address: volunteerAddress.value
+    }
+
+    try {
+        const res =await fetch('/api/v1/volunteers',{
+
+            'method':'POST',
+            'headers':{
+                'Content-Type':'application/json'
+            },
+            'body': JSON.stringify(sendBody)
+        });
+
+        if(res.status === 400){
+            throw Error('Volunteer already exists!')
+        }
+
+        alert('Volunteer added!');
+        window.location.href = '/index.html';
+    } catch (err) {
+        alert(err);
+        return;
+    }
+}
+
+volunteerForm.addEventListener('submit', addVolunteer)
--- connect-with-nodeJS/server.js
+++ connect-with-nodeJS/server.js
@@ -19,10 +19,13 @@ app.use(express.json());
 // Enable cors
 app.use(cors());
 
-// Routers
-app.use('/api/v1/volunteers', require('./routes/volunteers'))
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 5000;
+// Routers
+app.use('/api/v1/volunteers', require('./routes/volunteers'));
+
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
