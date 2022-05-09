82eb92e04829a5bb14b3839b1e59cc93509590e6
Niklas Lübcke <niklas.luebcke@innoplexia.com>
2022-05-09 14:02:11 +0200

    Adds start-stop to car-class.

--- gps-spoofer-rust/src/car.rs
+++ gps-spoofer-rust/src/car.rs
@@ -1,62 +1,85 @@
 use crate::paths::{calc_current_coords, progress_percent, traveled_distance};
-use geo::{prelude::HaversineLength, LineString};
+use geo::{prelude::HaversineLength, Coordinate, LineString, Point};
 use std::{
     ops::Sub,
     sync::mpsc::SyncSender,
     thread::{self, JoinHandle},
     time::Instant,
 };
-
+#[derive(Copy, Clone)]
 pub struct Car {
     pub car_type: &'static str,
-    pub nr: &'static str,
+    pub license_plate: &'static str,
+    pub speed: f64, // In m/s
+    pub driving_since: f64,
     pub progress: f64,
     pub traveled_distance: f64,
-    pub length: f64,
-    pub time_diff_in_s: f64,
+    pub path_length: f64,
+    pub lap_clock: f64,
     pub coords: (f64, f64),
+    stopped: bool,
 }
 
 impl Car {
     pub fn new(
         car_type: &'static str,
-        nr: &'static str,
+        license_plate: &'static str,
         speed: f64, // In m/s
-        infinite: bool,
-        tx: SyncSender<Car>,
         path: LineString<f64>,
+    ) -> Car {
+        let path_length = path.haversine_length().round();
+        let path_as_points = path.into_points();
+        let first_point = path_as_points.first().unwrap();
+
+        return Self {
+            car_type,
+            license_plate,
+            speed,
+            driving_since: 0.0,
+            progress: 0.0,
+            traveled_distance: 0.0,
+            path_length,
+            lap_clock: 0.0,
+            coords: (first_point.0.x, first_point.0.y),
+            stopped: false,
+        };
+    }
+
+    pub fn drive(
+        mut self,
+        path: LineString<f64>,
+        infinite: bool,
+        tx: SyncSender<Self>,
     ) -> JoinHandle<()> {
         let handle = thread::spawn(move || {
             let length = path.haversine_length().round();
-            let mut init_time = Instant::now();
+            let init_full_time = Instant::now();
+            let mut init_clock = Instant::now();
 
             loop {
-                let time_diff_in_s = Instant::now().sub(init_time).as_secs_f64(); // init_time.elapsed....
-                let traveled_distance = traveled_distance(speed, time_diff_in_s);
-                let progress = progress_percent(length, traveled_distance);
-                let new_coords = calc_current_coords(&path, length, time_diff_in_s, speed);
+                self.driving_since = init_full_time.elapsed().as_secs_f64();
+                if self.stopped {
+                    tx.send(self).unwrap();
+                } else {
+                    self.lap_clock = init_clock.elapsed().as_secs_f64();
+                    self.traveled_distance = traveled_distance(self.speed, self.lap_clock);
+                    self.progress = progress_percent(length, self.traveled_distance);
+                    let new_coords = calc_current_coords(&path, length, self.lap_clock, self.speed);
 
-                match new_coords {
-                    Ok(coords) => {
-                        let payload = Self {
-                            car_type,
-                            nr,
-                            progress,
-                            traveled_distance,
-                            length,
-                            time_diff_in_s,
-                            coords,
-                        };
+                    match new_coords {
+                        Ok(coords) => {
+                            self.coords = coords;
 
-                        tx.send(payload).unwrap();
-                    }
-                    Err(e) => {
-                        println!("\n{:#?}", e);
-                        if infinite {
-                            init_time = Instant::now();
-                        } else {
-                            break;
-                        };
+                            tx.send(self).unwrap();
+                        }
+                        Err(e) => {
+                            println!("\n{:#?}", e);
+                            if infinite {
+                                init_clock = Instant::now();
+                            } else {
+                                break;
+                            };
+                        }
                     }
                 }
             }
@@ -67,5 +90,9 @@ impl Car {
         });
 
         return handle;
+    }
+
+    pub fn toggle(mut self) {
+        self.stopped = !self.stopped
     }
 }
