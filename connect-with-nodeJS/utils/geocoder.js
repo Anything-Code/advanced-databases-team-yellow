a11b8268370b207feea98b50cde2a3084be478e0
Enes AGIR <enes.agir.7@gmail.com>
2022-05-12 06:22:11 +0200

    creating new API

--- connect-with-nodeJS/utils/geocoder.js
+++ connect-with-nodeJS/utils/geocoder.js
@@ -1,12 +1,12 @@
 const NodeGeocoder = require('node-geocoder');
 
 const options = {
-    provider: process.env.GEOCODER_PROVIDER,
+    provider: 'mapquest', //process.env.GEOCODER_PROVIDER,
     httpAdapter: 'https',
-    apiKey: process.env.GEOCODER_API_KEY,
+    apiKey: 'KjVN556l16xasyFiG0HYDaAyatfUilmb', //process.env.GEOCODER_API_KEY,
     formatter: null
 };
-
+console.log(options);
 const geocoder = NodeGeocoder(options);
 
 module.exports = geocoder;
