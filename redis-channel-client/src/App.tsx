848b58cbee71389405fef141deb506621d80ffff
Niklas Lübcke <niklas.luebcke@innoplexia.com>
2022-05-13 11:11:51 +0200

    Commits last changes.

--- redis-channel-client/src/App.tsx
+++ redis-channel-client/src/App.tsx
@@ -1,7 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { MapContainer, Marker, Polyline, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Polyline,
+  Popup,
+  TileLayer,
+} from "react-leaflet";
 import layers from "leaflet/dist/images/layers.png";
 import layers2x from "leaflet/dist/images/layers-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
@@ -18,9 +24,79 @@ export function sleep(ms: number) {
 
 function App() {
   const position = { lat: 49.4141772, lng: 8.6508929 };
+
+  const [policePos, setPolicePos] = useState<{
+    lat: number;
+    lng: number;
+  }>(position);
+  const [ambulancePos, setAmbulancePos] = useState<{
+    lat: number;
+    lng: number;
+  }>(position);
+  const [firetruckPos, setFiretruckPos] = useState<{
+    lat: number;
+    lng: number;
+  }>(position);
+  const [manhuntPos, setManhuntPos] = useState<{
+    lat: number;
+    lng: number;
+  }>({ lng: 8.6508929, lat: 49.4141772 });
+  const [stabbingPos, setStabbingPos] = useState<{
+    lat: number;
+    lng: number;
+  }>({ lng: 8.657238054886221, lat: 49.3784348 });
+  const [firehazardPos, setFirehazardPos] = useState<{
+    lat: number;
+    lng: number;
+  }>({ lng: 8.6839833, lat: 49.41273485 });
+  const [socket, setSocket] = useState<WebSocket>(
+    new WebSocket("ws://localhost:8080")
+  );
+  const [carsOccupied, setCarsOccupied] = useState<Record<string, boolean>>({
+    BWL_A_1: false,
+    BWL_A_2: false,
+    BWL_A_3: false,
+  });
+  const toggleJoin = useCallback(
+    (license_plate: string) => {
+      switch (license_plate) {
+        case "BWL_A_1":
+          if (carsOccupied.BWL_A_1) {
+            socket.send("Leave 1");
+            setCarsOccupied((old) => ({ ...old, BWL_A_1: false }));
+          } else {
+            socket.send("Join 1");
+            setCarsOccupied((old) => ({ ...old, BWL_A_1: true }));
+          }
+          break;
+        case "BWL_A_2":
+          if (carsOccupied.BWL_A_2) {
+            socket.send("Leave 2");
+            setCarsOccupied((old) => ({ ...old, BWL_A_2: false }));
+          } else {
+            socket.send("Join 2");
+            setCarsOccupied((old) => ({ ...old, BWL_A_2: true }));
+          }
+          break;
+        case "BWL_A_3":
+          if (carsOccupied.BWL_A_3) {
+            socket.send("Leave 3");
+            setCarsOccupied((old) => ({ ...old, BWL_A_3: true }));
+          } else {
+            socket.send("Join 3");
+            setCarsOccupied((old) => ({ ...old, BWL_A_3: true }));
+          }
+          break;
+
+        default:
+          break;
+      }
+    },
+    [carsOccupied.BWL_A_1, carsOccupied.BWL_A_2, carsOccupied.BWL_A_3, socket]
+  );
+
   useEffect(() => {
     try {
-      const socket = new WebSocket("ws://localhost:8080");
       socket.addEventListener("open", async (_event) => {
         socket.send("Client connected!");
         // await sleep(5000);
@@ -59,32 +135,7 @@ function App() {
     } catch (err) {
       console.error(`Could not connect to WebSocket: '${err}'`);
     }
-  }, []);
-
-  const [policePos, setPolicePos] = useState<{
-    lat: number;
-    lng: number;
-  }>(position);
-  const [ambulancePos, setAmbulancePos] = useState<{
-    lat: number;
-    lng: number;
-  }>(position);
-  const [firetruckPos, setFiretruckPos] = useState<{
-    lat: number;
-    lng: number;
-  }>(position);
-  const [manhuntPos, setManhuntPos] = useState<{
-    lat: number;
-    lng: number;
-  }>({ lng: 8.6508929, lat: 49.4141772 });
-  const [stabbingPos, setStabbingPos] = useState<{
-    lat: number;
-    lng: number;
-  }>({ lng: 8.657238054886221, lat: 49.3784348 });
-  const [firehazardPos, setFirehazardPos] = useState<{
-    lat: number;
-    lng: number;
-  }>({ lng: 8.6839833, lat: 49.41273485 });
+  }, [socket]);
 
   return (
     <MapContainer
@@ -130,9 +181,9 @@ function App() {
         <Popup>#Firehazard-1 8.6839833 49.41273485</Popup>
       </Marker>
 
-        <Polyline positions={[policePos, ambulancePos]} />
-        <Polyline positions={[ambulancePos, firetruckPos]} />
-        <Polyline positions={[firetruckPos, policePos]} />
+      <Polyline positions={[policePos, ambulancePos]} />
+      <Polyline positions={[ambulancePos, firetruckPos]} />
+      <Polyline positions={[firetruckPos, policePos]} />
 
       <Marker
         position={policePos}
@@ -144,7 +195,14 @@ function App() {
           })
         }
       >
-        <Popup>BWL_A_1</Popup>
+        <Popup>
+          <span style={{ padding: "1rem" }}>BWL_A_1</span>
+          {carsOccupied.BWL_A_1 ? (
+            <button onClick={() => toggleJoin("BWL_A_1")}>Leave Emergency #Manhunt-1</button>
+          ) : (
+            <button onClick={() => toggleJoin("BWL_A_1")}>Join Emergency #Manhunt-1</button>
+          )}
+        </Popup>
       </Marker>
 
       <Marker
@@ -157,7 +215,14 @@ function App() {
           })
         }
       >
-        <Popup>BWL_A_2</Popup>
+        <Popup>
+          <span style={{ padding: "1rem" }}>BWL_A_2</span>
+          {carsOccupied.BWL_A_2 ? (
+            <button onClick={() => toggleJoin("BWL_A_2")}>Leave Emergency #Stabbing-1</button>
+          ) : (
+            <button onClick={() => toggleJoin("BWL_A_2")}>Join Emergency #Stabbing-1</button>
+          )}
+        </Popup>
       </Marker>
 
       <Marker
@@ -170,7 +235,14 @@ function App() {
           })
         }
       >
-        <Popup>BWL_A_3</Popup>
+        <Popup>
+          <span style={{ padding: "1rem" }}>BWL_A_3</span>
+          {carsOccupied.BWL_A_3 ? (
+            <button onClick={() => toggleJoin("BWL_A_3")}>Leave Emergency #Firehazard-1</button>
+          ) : (
+            <button onClick={() => toggleJoin("BWL_A_3")}>Join Emergency #Firehazard-1</button>
+          )}
+        </Popup>
       </Marker>
     </MapContainer>
   );
